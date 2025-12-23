0918739ec2a908d2791f8e7724d55177fc82f08e
veslrs <sub.fang.zeyuan@outlook.com>
2025-12-23 20:32:10 +0100

    restructure...

--- chapel/0001.chpl
+++
@@ -1,8 +0,0 @@
-param lim = 1000;
-var sum = 0;
-
-for i in 1..lim - 1 {
-    if i % 3 == 0 || i % 5 == 0 then sum += i;
-}
-
-writeln("sum of all the multiples of 3 or 5 below 1000 = ", sum);
--- julia/0001.jl
+++
@@ -1,9 +0,0 @@
-function mul_3_5(lim::Int)
-    sum = 0
-    for i = 1:lim-1
-        if i % 3 == 0 || i % 5 == 0
-            sum +=i
-        end
-    end
-    println("sum of all the multiples of 3 or 5 below 1000 = ", sum)
-end
